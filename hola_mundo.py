c536903f3fdc6164cfd7052862faf0fd84d7d9fb
Miguel <123456789capacitacion.creativa4@gmail.noreply.github.com>
2026-06-25 19:37:24 -0600

    Agrego mi versión personalizada de hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,7 @@
+# Taller Git y GitHub - Comunidad FIUBA
+# Participante: Miguel Angel Calvo
+
 nombre = input("¿Cómo te llamás? ")
 print(f"¡Hola, {nombre}! Bienvenido al taller.")
+print("Soy Miguel Angel, jubilado y nuevo entusiasta de la IA.")
+print("Hoy aprendí que Git y GitHub no son solo para programadores.")
